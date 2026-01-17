683c4ac05d7812ef8bba2e2fecf93933b03fbc80
RagniNagpal <ragninagpal@gmail.com>
2026-01-17 21:23:23 +0530

    done

---
+++ src/DP/LongestCommonSub.java
@@ -0,0 +1,27 @@
+package DP;
+
+public class LongestCommonSub {
+    public int lcs(String a, String b){
+        int m=a.length();
+        int n=b.length();
+
+        int[][] dp=new int[n+1][m+1];
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(a.charAt(i-1)==b.charAt(j-1)){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+
+                }
+            }
+
+        }
+        return dp[n][m];
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
